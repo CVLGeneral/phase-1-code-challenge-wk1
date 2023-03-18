982a0fb65f1fe9ba4a833cadf21273dc01dc3647
Leonard Kiprotich <cvlgeneral2020@gmail.com>
2023-03-18 13:34:13 +0300

    Done with quiz 1

--- marks.js
+++ marks.js
@@ -1,21 +1,21 @@
 function marksScore() {
 
    const marks=parseInt(document.getElementById("mks").value);
-
+//It gets the value of input submitted by the user
 
 
 if (marks >= 0 && marks <= 100) {
-
+//checks if the input value lies in the range of 0-100
     if (marks>79&& marks<=100){
-        document.getElementById("grade").innerHTML="You Got A Grade";
+        document.getElementById("grade").innerHTML="Grade: A";  //if the condition is true it returns Grade A
     }else if(marks>59 && marks<=79){
-        document.getElementById("grade").innerHTML="You Got B Grade";
+        document.getElementById("grade").innerHTML="Grade: B";
     }else if(marks>49 && marks <=59) {
-        document.getElementById("grade").innerHTML="You Got C Grade";
+        document.getElementById("grade").innerHTML="Grade: C";
     } else if (marks>39 && marks<=49){
-        document.getElementById("grade").innerHTML="You Got D Grade";
+        document.getElementById("grade").innerHTML="Grade: D";
     }else {
-        document.getElementById("grade").innerHTML="You Got E Grade";
+        document.getElementById("grade").innerHTML="Grade: E";
     }
 
 } else{
