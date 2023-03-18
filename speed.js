c1b3ebf0014f5c36ca90050156dc60ea11948f89
Leonard Kiprotich <cvlgeneral2020@gmail.com>
2023-03-18 15:03:39 +0300

    Done with quiz 2

--- speed.js
+++ speed.js
@@ -1,6 +1,25 @@
-function speedDetector(speed){
+function speedDetector(){
+
+    const speed=parseInt(document.getElementById("spd").value);
+
     if (speed<70){
-        return "OK"
+        document.getElementById("speed").innerHTML="OK";  //if the condition is true it returns Grade A
+    } else{
+        const demeritPoints = (speed - 70) / 5;
+
+        if (demeritPoints<=12){
+
+        document.getElementById("speed").innerHTML=`Points: ${demeritPoints}`;  //if the condition is true it returns Grade A
+        
+        }else{
+
+            document.getElementById("speed").innerHTML="License Suspended"; 
+
+
+        }
+
     }
+    
+
 
 }
